76fd93fe4deabcbf3e1d8a3a0857145d6c546cb5
ShamliShahni <shamli.shahni@mhgtechnologies.com>
2021-10-13 22:04:45 +0530

    confirmation popup

--- Control.gd
+++ Control.gd
@@ -3,8 +3,10 @@ extends Control
 var style_box = preload("res://assets/new_styleboxflat.tres")
 var buttontex = preload("res://Images/yellow button.png")
 var new_style = StyleBoxFlat.new()
+var style2 = StyleBoxFlat.new()
+var dyn_font = DynamicFont.new()
 onready var panel = get_node("Panel3")
-onready var button = get_node("Button")
+
 
 func _ready():
 	new_style.bg_color = Color.aqua
@@ -16,17 +18,19 @@ func _ready():
 	new_style.border_width_right = 5
 	new_style.shadow_color = Color.darkblue
 	new_style.shadow_size = 5
-	#new_style.border_blend = true
+	style2.bg_color = Color.red
+	dyn_font.font_data = load("res://fonts/Xolonium-Regular.ttf")
+	dyn_font.size = 120
 	
 	panel.set('custom_styles/panel', new_style)
-
+	
+	
 
 func _on_Button_pressed():
 	print("click")
 
 
 func _on_Button2_pressed():
-	#print("222")
 	new_style.corner_radius_bottom_left += 1
 	new_style.corner_radius_bottom_right += 1
 	new_style.corner_radius_top_left += 1
@@ -36,21 +40,53 @@ func _on_Button2_pressed():
 	
 
 func _on_Timer_timeout():
-	
-	#ctext.texture_normal = load("res://Images/yellow button.png")
-	$ConfirmationDialog.window_title = "Hello"
-	$ConfirmationDialog.add_icon_override("normal",buttontex)
-	$ConfirmationDialog.add_stylebox_override("panel",new_style)
-	#$ConfirmationDialog.add_stylebox_override("cancel_button",new_style)
-	#$ConfirmationDialog.add_stylebox_override("close",new_style)
+	#dec_confirmbox()
 	$ConfirmationDialog.popup()
-	var ctext = $ConfirmationDialog.get_close_button()
-	#ctext.texture_normal = buttontex
+	dec_confirmbox()
+	
 
-func _process(delta):
-	pass
-
-
-func _on_Timer2_timeout():
-	$WindowDialog.add_stylebox_override('panel',style_box)
-	$WindowDialog.popup()
+func dec_confirmbox():
+#
+#	$ConfirmationDialog.add_font_override('font',dyn_font)
+#	$ConfirmationDialog.add_color_override("font_color", Color.red)
+	#$ConfirmationDialog.set('title_color',Color.red)
+	$ConfirmationDialog.show_behind_parent = true
+	print($ConfirmationDialog.show_behind_parent)
+	print($ConfirmationDialog.get_color('title_color'))
+	
+	$ConfirmationDialog.add_color_override('title_color',Color.aquamarine)
+	print($ConfirmationDialog.get_color('title_color'))
+	$WindowDialog.add_color_override('title_color',Color.red)
+	#$WindowDialog.popup()
+	$ConfirmationDialog.window_title = "Hello"
+	var labeltext = $ConfirmationDialog.get_child(1)
+	labeltext.add_font_override("font",dyn_font)
+	labeltext.add_color_override("font_color",Color.red)
+	#$ConfirmationDialog.add_icon_override("normal",buttontex)
+	$ConfirmationDialog.add_stylebox_override("panel",new_style)
+	var okbut = $ConfirmationDialog.get_ok()
+	var ctext = $ConfirmationDialog.get_cancel()
+	ctext.set('rect_size',Vector2(300,300))
+	#ctext.rect_size = Vector2(200,200)
+	#ctext.rect_position = Vector2(0,-500)
+	#print(ctext.rect_position)
+	#okbut.set('custom_styles/normal', style2)
+	okbut.set('custom_styles/focus', style2)
+	okbut.set('custom_styles/hover', style2)
+	okbut.set('custom_styles/pressed', style2)
+	ctext.set('custom_styles/normal', style2)
+	ctext.set('custom_styles/focus', style2)
+	ctext.set('custom_styles/hover', style2)
+	ctext.set('custom_styles/pressed', style2)
+	print($ConfirmationDialog.get_child_count())
+	print($ConfirmationDialog.get_children())
+	var lb = $ConfirmationDialog.get_label()
+	lb.add_stylebox_override('normal',new_style)
+	
+	
+	for item in $ConfirmationDialog.get_children():
+		print(item.name)
+		if(item.name=="@@4"):
+			for child in item.get_children():
+				print(child)
+		pass
